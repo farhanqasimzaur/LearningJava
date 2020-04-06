ba3dab85c99c25b914bba0b661828fc3e755d9e7
Zaira Zafar <zaira.zafar@venturedive.com>
2020-04-06 15:31:46 +0500

    [java-sql] created an explain function in understanding java moved java101 code to understanding java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,35 +1,11 @@
 package com.company;
 
-import com.company.java101.CollectionsExample;
 import com.company.java101.UnderstandingJava;
-import com.company.java101.oop.Frog;
-
-import java.util.HashMap;
-import java.util.LinkedList;
 
 public class Main {
 
     public static void main(String[] args) {
-        UnderstandingJava myLearning = new UnderstandingJava();
-        myLearning.showCat();
 
-        System.out.println("");
-        myLearning.cat.addToys("stuffed mouse");
-        myLearning.cat.addToys("electronic cockroach");
-        myLearning.cat.catToys();
-        System.out.println("");
-        myLearning.cat.setOldToys();
-        myLearning.cat.showOldToys();
 
-        myLearning.generics();
-        System.out.println("");
-        myLearning.mutateCat(myLearning.cat, 3);
-        myLearning.mutateCat(myLearning.dog, 2);
-        Frog frog = new Frog();
-        myLearning.mutateCat(frog, null);
-        myLearning.mutateSound("");
-
-        System.out.println("");
-        CollectionsExample collection = new CollectionsExample();
     }
 }
--- src/com/company/java101/UnderstandingJava.java
+++ src/com/company/java101/UnderstandingJava.java
@@ -22,6 +22,29 @@ public class UnderstandingJava {
 
     }
 
+    public void explain() {
+        UnderstandingJava myLearning = new UnderstandingJava();
+        myLearning.showCat();
+
+        System.out.println("");
+        myLearning.cat.addToys("stuffed mouse");
+        myLearning.cat.addToys("electronic cockroach");
+        myLearning.cat.catToys();
+        System.out.println("");
+        myLearning.cat.setOldToys();
+        myLearning.cat.showOldToys();
+
+        myLearning.generics();
+        System.out.println("");
+        myLearning.mutateCat(myLearning.cat, 3);
+        myLearning.mutateCat(myLearning.dog, 2);
+        Frog frog = new Frog();
+        myLearning.mutateCat(frog, null);
+        myLearning.mutateSound("");
+
+        System.out.println("");
+        CollectionsExample collection = new CollectionsExample();
+    }
 //    OOP
     public void showCat() {
         cat.showCatBehaviour();
