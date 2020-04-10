726c994deee489e65f9e3885ad768da18274b405
Zaira Zafar <zaira.zafar@venturedive.com>
2020-04-10 18:34:15 +0500

    [hibernate] hibernate dependency added

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,16 +1,8 @@
 package com.company;
 
-import com.company.connectingMySQL.Students;
-import com.company.restfulAPIs.StudentsService;
-
 public class Main {
 
     public static void main(String[] args) {
-//        Students student = new Students();
-//        student.create("hello", 20);
-//        student.delete(7);
-//        student.create("new", 20);
-//        student.updateAge(1, 50);
-//        student.getAll();
+
     }
 }
